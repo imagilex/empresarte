cf6ebc65d78c3c7a22c03927126b3ebb22715ba4
Rubén Ramírez <rramirez@junkyard.mx>
2018-03-10 00:24:34 -0600

    Preparación para Personalización

--- wp-content/themes/simple-theme/footer.php
+++ wp-content/themes/simple-theme/footer.php
@@ -1,10 +1,124 @@
 		</div><!-- #main-container -->
 
+		<nav id="social-navbar">
+			<ul>
+				<li><!-- Instagram -->
+					<a href="https://www.instagram.com/rramirezxd/" target="_blank">
+						<i class="fab fa-instagram fa-2x"></i>
+						<span class="field">Instagram</span>
+						<span class="data">rramirezxd</span>
+					</a>
+				</li>
+				<li><!-- Facebook -->
+					<a href="https://www.facebook.com/rramirez0202" target="_blank">
+						<i class="fab fa-facebook-f fa-2x"></i>
+						<span class="field">Facebook</span>
+						<span class="data">rramirez0202</span>
+					</a>
+				</li>
+				<li><!-- Twitter -->
+					<a href="https://twitter.com/imagilex" target="_blank">
+						<i class="fab fa-twitter fa-2x"></i>
+						<span class="field">Twitter</span>
+						<span class="data">@imagilex</span>
+					</a>
+				</li>
+				<li><!-- Youtube -->
+					<a href="https://www.youtube.com/channel/UC3BOlZPi13YMREKHYkJPTAA" target="_blank">
+						<i class="fab fa-youtube fa-2x"></i>
+						<span class="field">YouTube</span>
+						<span class="data">Mi Canal YT</span>
+					</a>
+				</li>
+				<li><!-- Vimeo -->
+					<a href="https://vimeo.com/user82670805" target="_blank">
+						<i class="fab fa-vimeo-v fa-2x"></i>
+						<span class="field">Vimeo</span>
+						<span class="data">Mi Canal Vimeo</span>
+					</a>
+				</li>
+				<li><!-- Pinterest -->
+					<a href="https://www.pinterest.com.mx/matemerk/" target="_blank">
+						<i class="fab fa-pinterest fa-2x"></i>
+						<span class="field">Pinterest</span>
+						<span class="data">matemerk</span>
+					</a>
+				</li>
+				<li><!-- Vine -->
+					<a href="https://vine.co/u/940474327508377600" target="_blank">
+						<i class="fab fa-vine fa-2x"></i>
+						<span class="field">Vine</span>
+						<span class="data">Mi canal Vine</span>
+					</a>
+				</li>
+				<li><!-- LinkedIn -->
+					<a href="https://www.linkedin.com/in/rramirez0202/" target="_blank">
+						<i class="fab fa-linkedin-in fa-2x"></i>
+						<span class="field">LinkedIn</span>
+						<span class="data">rramirez0202</span>
+					</a>
+				</li>
+				<li><!-- Google Plus -->
+					<a href="https://plus.google.com/u/0/+RubenRG0202" target="_blank">
+						<i class="fab fa-google-plus-g fa-2x"></i>
+						<span class="field">Google Plus</span>
+						<span class="data">RubenRG0202</span>
+					</a>
+				</li>
+				<li><!-- EMail -->
+					<a href="mailto:rramirez@rramirez.com" target="_blank">
+						<i class="fas fa-envelope fa-2x"></i>
+						<span class="field">Correo</span>
+						<span class="data">rramirez@rramirez.com</span>
+					</a>
+				</li>
+				<li><!-- Teléfono -->
+					<a href="tel:7715698549" target="_blank">
+						<i class="fas fa-phone fa-2x"></i>
+						<span class="field">Teléfono</span>
+						<span class="data">7715698549</span>
+					</a>
+				</li>
+				<li><!-- WhatsApp -->
+					<a href="https://api.whatsapp.com/send?phone=527715698549" target="_blank">
+						<i class="fab fa-whatsapp fa-2x"></i>
+						<span class="field">WhasApp</span>
+						<span class="data">7715698549</span>
+					</a>
+				</li>
+				<li><!-- Skype Chat -->
+					<a href="skype:rramirez_6?chat" target="_blank">
+						<i class="fab fa-skype fa-2x"></i>
+						<span class="field">Chat</span>
+						<span class="data">rramirez_6</span>
+					</a>
+				</li>
+				<li><!-- Skype Call -->
+					<a href="skype:rramirez_6?call" target="_blank">
+						<i class="fab fa-skype fa-2x"></i>
+						<span class="field">Video-llamada</span>
+						<span class="data">rramirez_6</span>
+					</a>
+				</li>
+				<li><!-- Facebook messenger -->
+					<a href="https://m.me/rramirez0202" target="blank">
+						<i class="fab fa-facebook-messenger fa-2x"></i>
+						<span class="field">Messenger</span>
+						<span class="data">rramirez0202</span>
+					</a>
+				</li>
+				<li><!-- Direccion -->
+					<i class="fas fa-globe fa-2x"></i>
+					<span class="field">Dirección</span>
+					<span class="data">Priv. de la Huerta 154<br />Rancho Don Antonio<br />Tizayuca, Hidalgo</span>
+				</li>
+			</ul>
+		</nav>
+
 		<footer id="main-footer">
 			<h4><?php bloginfo( 'name' ); ?>, 2018</h4>
 		</footer><!-- #main-footer -->
 
 		<?php wp_footer(); ?>
-
 	</body>
 </html>
